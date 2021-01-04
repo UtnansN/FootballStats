183d58b5443a2aadc7687944f6b4485cc1f8350d
UtnansN <nauris.utnans@gmail.com>
2021-01-04 20:07:53 +0200

    Add project files.

---
+++ FootballStats/AddGameWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Windows;
+
+namespace FootballStats
+{
+    /// <summary>
+    /// Interaction logic for AddGameWindow.xaml
+    /// </summary>
+    public partial class AddGameWindow : Window
+    {
+
+        public AddGameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach(string name in openFileDialog.FileNames)
+                {
+                    JObject obj;
+                    using (StreamReader reader = File.OpenText(name))
+                    {
+                        obj = (JObject) JToken.ReadFrom(new JsonTextReader(reader))["Spele"];
+                    }
+                    GameNamesBox.Items.Add(name);
+                }
+            }
+
+        }
+    }
+}
---
+++ FootballStats/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStats.Entities
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public DateTime date { get; set; } 
+        public int viewerCount { get; set; }
+        public String place { get; set; }
+
+    }
+}
